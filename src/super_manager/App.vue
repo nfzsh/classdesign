0add01fae2c09afb0fadc72cfe3a120cc2997776
1016297824 <1016297824@qq.com>
2019-06-09 15:12:13 +0800

    ClassDesign

--- src/super_manager/App.vue
+++ src/super_manager/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="super_mnger">
+    <mian />
     <alert />
   </div>
 </template>
@@ -9,6 +10,7 @@ import { init } from "@/super_manager/js/super_manager";
 
 export default {
   components: {
+    mian: () => import("@/super_manager/view/main"),
     alert: () => import("@/components/AlertDialog")
   },
   data: () => ({
